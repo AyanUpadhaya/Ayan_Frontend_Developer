03c3af6a1264a53eb4933e73be7430d21c641595
Ayan Upadhaya <ayanu881@gmail.com>
2023-07-18 15:42:22 +0600

    All api are now secured

--- server/capsules.php
+++ server/capsules.php
@@ -2,12 +2,21 @@
     require_once 'config.php';
     require_once 'enviroment.php';
     require 'apiHandler.php';
+    $authToken = apache_getenv('ACESSTOKEN');
+    
 
     // Route the requests
     if($_SERVER['REQUEST_METHOD']==="GET"){
-        $api_handler = new APIHandler('https://api.spacexdata.com/v4/capsules');
-        $capsules = $api_handler->getAllData();
-        echo json_encode($capsules);
+        $headers = apache_request_headers();
+        if(isset($headers['Authorization']) && $headers['Authorization']===$authToken){
+            $api_handler = new APIHandler('https://api.spacexdata.com/v4/capsules');
+            $capsules = $api_handler->getAllData();
+            echo json_encode($capsules);
+        }else{
+            http_response_code(401);
+            echo json_encode(["error"=>"Unauthorized Access"]);
+        }
+        
     }else{
         http_response_code(405);
         echo json_encode(["error"=>"Method Not allowed"]);
--- server/crews.php
+++ server/crews.php
@@ -2,11 +2,19 @@
     require_once 'config.php';
     require_once 'enviroment.php';
     require 'apiHandler.php';
+    $authToken = apache_getenv('ACESSTOKEN');
 
     if($_SERVER['REQUEST_METHOD']==="GET"){
-        $api_handler = new APIHandler('https://api.spacexdata.com/v4/crew');
-        $crews = $api_handler->getAllData();
-        echo json_encode($crews);
+        $headers = apache_request_headers();
+        if(isset($headers['Authorization']) && $headers['Authorization'] === $authToken){
+            $api_handler = new APIHandler('https://api.spacexdata.com/v4/crew');
+            $crews = $api_handler->getAllData();
+            echo json_encode($crews);
+        }else{
+            http_response_code(401);
+            echo json_encode(["error"=>"Unauthorized Access"]);
+        }
+        
     }else{
         http_response_code(405);
         echo json_encode(["error"=>"Method Not allowed"]);
